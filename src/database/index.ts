2f6396a288ffff596e09454049b863ea75906887
Luccas Specht <luccasspecht70@gmail.com>
2022-02-23 21:15:16 -0300

    integration test: add inicial configuration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,13 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-(async () => await createConnection())();
+export default async (host = "database"): Promise<Connection> => {
+  const defaultOption = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOption, {
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test" ? "fin_api" : defaultOption.database,
+    })
+  );
+};
